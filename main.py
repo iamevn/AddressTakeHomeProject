7aebc7ada6b4df2c96d3a8c1eb5de9818676d898
iamevn <iamevn@users.noreply.github.com>
2022-12-04 15:49:21 -0800

    Read data out of CSVs and into dicts
    csv files can be provided as command line args or data can be provided directly on stdin
    
    just prints the data for now

---
+++ main.py
@@ -0,0 +1,45 @@
+import fileinput
+import sys
+import csv
+
+HELP_TEXT = f"""
+Extract household and occupant info from provided data files.
+
+Data files should be provided either as arguments.
+If no files are specified data will be read from stdin.
+
+Data files are expected to be CSVs with fields for:
+
+  first name, last name, street address, city, state, age
+
+"""
+
+CSV_FIELDS = [
+  'firstname',
+  'lastname',
+  'street address',
+  'city',
+  'state',
+  'age',
+]
+
+def main():
+  # fileinput handles opening files specified as command line args
+  # and reading from stdin when no args are provided
+  with fileinput.input() as f:
+    reader = csv.DictReader(f, fieldnames=CSV_FIELDS)
+    for row in reader:
+      print(row)
+
+
+def needsHelp() -> bool:
+  args = set(sys.argv[1:])
+  return '--help' in args or '-h' in args
+    
+
+if __name__ == '__main__':
+  if needsHelp():
+    sys.stderr.write(HELP_TEXT)
+    sys.exit(1)
+  
+  main()
