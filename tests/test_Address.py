4931ca1fb03ed05384c129b3d9498b4e14553d70
iamevn <iamevn@users.noreply.github.com>
2022-12-04 19:42:10 -0800

    clean up mixed double and single quotes

--- tests/test_Address.py
+++ tests/test_Address.py
@@ -8,47 +8,47 @@ from household import Address
 class TestEquality(unittest.TestCase):
 
   def test_simple_equals(self):
-    A = Address("123 Example St", "Seattle", "WA")
-    B = Address("123 Example St", "Seattle", "WA")
+    A = Address('123 Example St', 'Seattle', 'WA')
+    B = Address('123 Example St', 'Seattle', 'WA')
     self.assertEqual(A, A)
     self.assertEqual(A, B)
 
   def test_simple_not_equals(self):
-    A = Address("123 Example St", "Seattle", "WA")
-    B = Address("456 Some Other Lane", "San Francisco", "CA")
+    A = Address('123 Example St', 'Seattle', 'WA')
+    B = Address('456 Some Other Lane', 'San Francisco', 'CA')
     self.assertNotEqual(A, B)
 
   def test_simple_not_equals_street(self):
-    A = Address("123 Example St", "Seattle", "WA")
-    B = Address("456 Some Other Lane", "Seattle", "WA")
+    A = Address('123 Example St', 'Seattle', 'WA')
+    B = Address('456 Some Other Lane', 'Seattle', 'WA')
     self.assertNotEqual(A, B)
 
   def test_simple_not_equals_city(self):
-    A = Address("123 Example St", "Seattle", "WA")
-    B = Address("123 Example St", "San Francisco", "WA")
+    A = Address('123 Example St', 'Seattle', 'WA')
+    B = Address('123 Example St', 'San Francisco', 'WA')
     self.assertNotEqual(A, B)
 
   def test_simple_not_equals_state(self):
-    A = Address("123 Example St", "Seattle", "WA")
-    B = Address("123 Example St", "Seattle", "CA")
+    A = Address('123 Example St', 'Seattle', 'WA')
+    B = Address('123 Example St', 'Seattle', 'CA')
     self.assertNotEqual(A, B)
 
 
 class TestHashability(unittest.TestCase):
 
   def test_address_hashable(self):
-    A = Address("123 Example St", "Seattle", "WA")
-    B = Address("456 Some Other Lane", "Seattle", "WA")
-    C = Address("123 Example St", "San Francisco", "WA")
-    D = Address("123 Example St", "Seattle", "CA")
-    E = Address("456 Some Other Lane", "San Francisco", "CA")
+    A = Address('123 Example St', 'Seattle', 'WA')
+    B = Address('456 Some Other Lane', 'Seattle', 'WA')
+    C = Address('123 Example St', 'San Francisco', 'WA')
+    D = Address('123 Example St', 'Seattle', 'CA')
+    E = Address('456 Some Other Lane', 'San Francisco', 'CA')
     addresses = {A, B, C, D, E}
     self.assertEqual(len(addresses), 5)
     self.assertTrue(A in addresses)
 
   def test_address_hash_stable(self):
-    A = Address("123 Example St", "Seattle", "WA")
-    B = Address("456 Some Other Lane", "Seattle", "WA")
+    A = Address('123 Example St', 'Seattle', 'WA')
+    B = Address('456 Some Other Lane', 'Seattle', 'WA')
     self.assertEqual(len({A, B, A, A, A, B}), 2)
 
 
--- tests/test_Person.py
+++ tests/test_Person.py
@@ -8,58 +8,58 @@ from household import Address, Person
 class TestEquality(unittest.TestCase):
 
   def setUp(self):
-    self.addressA = Address("123 Example St", "Seattle", "WA")
-    self.addressB = Address("456 Some Other Lane", "San Francisco", "CA")
+    self.addressA = Address('123 Example St', 'Seattle', 'WA')
+    self.addressB = Address('456 Some Other Lane', 'San Francisco', 'CA')
 
   def test_simple_equals(self):
-    A = Person("John", "Doe", 35, self.addressA)
-    B = Person("John", "Doe", 35, self.addressA)
+    A = Person('John', 'Doe', 35, self.addressA)
+    B = Person('John', 'Doe', 35, self.addressA)
     self.assertEqual(A, A)
     self.assertEqual(A, B)
 
   def test_simple_not_equals(self):
-    A = Person("John", "Doe", 35, self.addressA)
-    B = Person("Jane", "Smith", 15, self.addressB)
+    A = Person('John', 'Doe', 35, self.addressA)
+    B = Person('Jane', 'Smith', 15, self.addressB)
     self.assertNotEqual(A, B)
 
   def test_not_equals_same_address(self):
-    A = Person("John", "Doe", 35, self.addressA)
-    B = Person("Jane", "Smith", 15, self.addressA)
+    A = Person('John', 'Doe', 35, self.addressA)
+    B = Person('Jane', 'Smith', 15, self.addressA)
     self.assertNotEqual(A, B)
 
   def test_not_equals_different_address(self):
-    A = Person("John", "Doe", 35, self.addressA)
-    B = Person("John", "Doe", 35, self.addressB)
+    A = Person('John', 'Doe', 35, self.addressA)
+    B = Person('John', 'Doe', 35, self.addressB)
     self.assertNotEqual(A, B)
 
 
 class TestAge(unittest.TestCase):
 
   def setUp(self):
-    self.address = Address("123 Example St", "Seattle", "WA")
+    self.address = Address('123 Example St', 'Seattle', 'WA')
 
   def test_isAdult(self):
-    self.assertTrue(Person("John", "Doe", 9999, self.address).isAdult())
-    self.assertTrue(Person("John", "Doe", 35, self.address).isAdult())
-    self.assertTrue(Person("John", "Doe", 19, self.address).isAdult())
-    self.assertTrue(Person("John", "Doe", 18, self.address).isAdult())
+    self.assertTrue(Person('John', 'Doe', 9999, self.address).isAdult())
+    self.assertTrue(Person('John', 'Doe', 35, self.address).isAdult())
+    self.assertTrue(Person('John', 'Doe', 19, self.address).isAdult())
+    self.assertTrue(Person('John', 'Doe', 18, self.address).isAdult())
 
   def test_not_isAdult(self):
-    self.assertFalse(Person("John", "Doe", 17, self.address).isAdult())
-    self.assertFalse(Person("John", "Doe", 16, self.address).isAdult())
-    self.assertFalse(Person("John", "Doe", 1, self.address).isAdult())
-    self.assertFalse(Person("John", "Doe", 0, self.address).isAdult())
+    self.assertFalse(Person('John', 'Doe', 17, self.address).isAdult())
+    self.assertFalse(Person('John', 'Doe', 16, self.address).isAdult())
+    self.assertFalse(Person('John', 'Doe', 1, self.address).isAdult())
+    self.assertFalse(Person('John', 'Doe', 0, self.address).isAdult())
 
 
 class TestOrdering(unittest.TestCase):
 
   def setUp(self):
-    self.address = Address("123 Example St", "Seattle", "WA")
+    self.address = Address('123 Example St', 'Seattle', 'WA')
 
   def test_lastname_ordering(self):
-    A = Person("John", "A", 35, self.address)
-    B = Person("John", "B", 35, self.address)
-    C = Person("John", "C", 35, self.address)
+    A = Person('John', 'A', 35, self.address)
+    B = Person('John', 'B', 35, self.address)
+    C = Person('John', 'C', 35, self.address)
 
     self.assertLess(A, B)
     self.assertLess(A, C)
@@ -74,9 +74,9 @@ class TestOrdering(unittest.TestCase):
     self.assertListEqual(sorted([C, A]), [A, C])
 
   def test_firstname_ordering(self):
-    A = Person("A", "Smith", 35, self.address)
-    B = Person("B", "Smith", 35, self.address)
-    C = Person("C", "Smith", 35, self.address)
+    A = Person('A', 'Smith', 35, self.address)
+    B = Person('B', 'Smith', 35, self.address)
+    C = Person('C', 'Smith', 35, self.address)
 
     self.assertLess(A, B)
     self.assertLess(A, C)
@@ -91,9 +91,9 @@ class TestOrdering(unittest.TestCase):
     self.assertListEqual(sorted([C, A]), [A, C])
 
   def test_combined_ordering(self):
-    A = Person("Jane", "Doe", 28, self.address)
-    B = Person("John", "Doe", 27, self.address)
-    C = Person("Jane", "Smith", 15, self.address)
+    A = Person('Jane', 'Doe', 28, self.address)
+    B = Person('John', 'Doe', 27, self.address)
+    C = Person('Jane', 'Smith', 15, self.address)
 
     self.assertLess(A, B)
     self.assertLess(A, C)
