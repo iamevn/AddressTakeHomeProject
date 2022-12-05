a3fecb2a9293b7259a32ad030912fdefd949f331
iamevn <iamevn@users.noreply.github.com>
2022-12-04 17:06:23 -0800

    test hashability of Addresses

--- tests/test_Address.py
+++ tests/test_Address.py
@@ -30,3 +30,22 @@ class TestEquality(unittest.TestCase):
     A = Address("123 Example St", "Seattle", "WA")
     B = Address("123 Example St", "Seattle", "CA")
     self.assertNotEqual(A, B)
+
+
+class TestHashability(unittest.TestCase):
+
+  def test_address_hashable(self):
+    A = Address("123 Example St", "Seattle", "WA")
+    B = Address("456 Some Other Lane", "Seattle", "WA")
+    C = Address("123 Example St", "San Francisco", "WA")
+    D = Address("123 Example St", "Seattle", "CA")
+    E = Address("456 Some Other Lane", "San Francisco", "CA")
+    addresses = {A, B, C, D, E}
+    self.assertEqual(len(addresses), 5)
+    self.assertTrue(A in addresses)
+
+  def test_address_hash_stable(self):
+    A = Address("123 Example St", "Seattle", "WA")
+    B = Address("456 Some Other Lane", "Seattle", "WA")
+    self.assertEqual(len({A, B, A, A, A, B}), 2)
+  
